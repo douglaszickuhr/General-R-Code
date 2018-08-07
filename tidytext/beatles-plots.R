e0f7b605a7c7c1dfa5e9bbea9a7db0d588d02e1b
Douglas Zickuhr <douglasrsl@hotmail.com>
2018-08-07 09:31:48 +0100

    New plots based on count and tf idf

--- tidytext/beatles-plots.R
+++ tidytext/beatles-plots.R
@@ -4,22 +4,37 @@ library(geniusR)
 
 lyrics <- read_csv("tidytext/data/beatles_lyrics.csv")
 
+x <- c("please please me", "with the beatles", "a hard day's night", "beatles for sale",
+       "help","rubber soul","revolver","sgt pepper's lonely hearts club band", "magical mystery tour",
+       "the beatles (the white album)","yellow submarine", "abbey road", "let it be") 
+
+lyrics <- lyrics %>%
+  mutate(album_title = fct_relevel(album_title, !!!x))
+
+lyrics %>%
+  anti_join(stop_words) %>%
+  count(word,sort = TRUE) %>%
+  top_n(20) %>%
+  ggplot() +
+  geom_col(aes(x=reorder(word,n),y=n)) + 
+  coord_flip()
 
 top_words <- lyrics %>%
   filter(!is.na(word)) %>%
   anti_join(stop_words) %>%
-  count(album_title,word,sort = TRUE)
+  count(album_title,word,sort = TRUE) %>%
+  arrange(album_title,desc(n))
 
+top_words
+
+rev(unique(top_words$word))
 
 
 top_words %>%
-  filter(!is.na(word)) %>%
   group_by(album_title) %>%
   top_n(10) %>%
-  mutate(word = fct_inorder(word,ordered = NA)) %>%
-  ungroup() %>%
-  ggplot(aes(x=word,y=n)) +
-  geom_col() + 
+  ggplot() +
+  geom_col(aes(x=reorder(word,n),y=n)) + 
   facet_wrap(~album_title, scales = "free") + 
   coord_flip()
 
@@ -29,11 +44,10 @@ relevant_words <- top_words %>%
 
 
 relevant_words %>%
-  mutate(word = factor(word, levels = rev(unique(word)))) %>%
   group_by(album_title) %>%
   top_n(10) %>%
   ungroup() %>%
-  ggplot(aes(word,tf_idf)) + 
+  ggplot(aes(reorder(word,tf_idf),tf_idf)) + 
   geom_col(show.legend = FALSE) + 
   facet_wrap(~album_title, scales = "free") + 
   coord_flip()
